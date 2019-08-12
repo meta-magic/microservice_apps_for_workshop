1099ec996503ce0f59dc077e71a1ba386f1c17df
ketangote <ketan.gote@metamagic.in>
2019-08-12 14:31:40 +0530

    Changes: Added Health API Calls

---
+++ productreview/src/main/java/com/metamagic/productreview/controller/HealthAPI.java
@@ -0,0 +1,17 @@
+package com.metamagic.productreview.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/check")
+public class HealthAPI {
+	
+	@GetMapping(value = "/live")
+	public ResponseEntity<String> checklive(){
+		return new ResponseEntity<String>("App is Live", HttpStatus.OK);
+	}
+}
