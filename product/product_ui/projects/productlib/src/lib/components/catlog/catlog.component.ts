cd37102fca68a3b40f73b90371eeb77c7f556513
datsgawas <dattaram.gawas@metamagic.in>
2019-08-13 19:16:10 +0530

    add to cart rest call integrated

--- product/product_ui/projects/productlib/src/lib/components/catlog/catlog.component.ts
+++ product/product_ui/projects/productlib/src/lib/components/catlog/catlog.component.ts
@@ -8,6 +8,7 @@ import {HttpService} from '../../services/http.service';
 import {SERVICE_URL} from '../../constant/service.constant';
 import {CommonService} from '../../services/common.service';
 import {Product} from "../../models/product.model";
+import {CookieService} from "ngx-cookie-service";
 
 @Component({
   selector: 'app-catlog',
@@ -23,6 +24,7 @@ export class CatlogComponent implements OnInit {
               private _httpClient: HttpClient,
               private _httpService: HttpService,
               public _cService: CommonService,
+              private _cookieService: CookieService,
               private store: Store<ProductNamespace.IProduct>) {
     this.store.pipe(select(ProductNamespace.getState)).subscribe((productState: any) => {
       if (productState) {
@@ -44,8 +46,19 @@ export class CatlogComponent implements OnInit {
         });
   }
 
-  addToCartHandle(product: any) {
-    this.store.dispatch(new AddToCart(product));
+  addToCartHandle(product: Product) {
+    debugger;
+    const requestObject = {};
+          requestObject['userId'] = this._cookieService.get('tokenId');
+          requestObject['productId'] = product.id;
+          requestObject['price'] = product.price;
+          requestObject['qty'] = 1;
+
+    this._httpService.restCall(SERVICE_URL.ADD_TO_CART, 'post', requestObject).toPromise()
+        .then((res: any) => {
+          this._cService.showLoader = false;
+          this.store.dispatch(new AddToCart(res.data));
+        });
   }
 
   productDetailsHandle(product: Product) {
--- product/product_ui/projects/productlib/src/lib/constant/service.constant.ts
+++ product/product_ui/projects/productlib/src/lib/constant/service.constant.ts
@@ -5,5 +5,6 @@ export const BASE_URl = 'http://35.227.30.18/';
 export const SERVICE_URL = {
   ADD_PRODUCT : BASE_URl + 'productms/api/product/',
   GET_ALL_PRODUCT : BASE_URl + 'productms/api/product/',
+  ADD_TO_CART: BASE_URl + 'shoppingcartms/api/shoppingcart/',
 };
 
