32a097d12cc253384e511d3148400b7b8b8d9055
datsgawas <dattaram.gawas@metamagic.in>
2019-08-14 18:06:01 +0530

    auth module added.

--- shoppingportal-ui-shell/src/app/app-routing.module.ts
+++ shoppingportal-ui-shell/src/app/app-routing.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {LoginComponent} from './components/login/login.component';
 import {HomeComponent} from "./components/home/home.component";
-import {AddUserComponent} from "./components/adduser/adduser.component"
 const routes: Routes = [
-
   {
     path: '', redirectTo: 'login', pathMatch: 'full'
   },
   {
-    path : 'login', component: LoginComponent
-  },
-  {
-    path : 'add-user', component: AddUserComponent
+    path : 'login', loadChildren: './modules/auth.module#AuthModule'
   },
   {
     path : 'home', component: HomeComponent,
---
+++ shoppingportal-ui-shell/src/app/modules/auth.module.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by dattaram on 12/8/19.
+ */
+import {NgModule} from '@angular/core';
+import {AuthLibModule} from "authlib";
+
+
+@NgModule({
+  imports: [AuthLibModule]
+})
+export class AuthModule {
+}
