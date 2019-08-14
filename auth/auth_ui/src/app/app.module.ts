706e7f45089683110ee69960d25942a901158a6c
datsgawas <dattaram.gawas@metamagic.in>
2019-08-14 19:12:02 +0530

    auth run standalone

--- auth/auth_ui/src/app/app.module.ts
+++ auth/auth_ui/src/app/app.module.ts
@@ -1,16 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import {CommonModule} from "@angular/common";
+
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import {AmexioWidgetModule} from 'amexio-ng-extensions';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {SharedlibModule} from "sharedlib";
+import {RouterModule, Routes} from "@angular/router";
+const routes: Routes = [
+  {
+    path : '', loadChildren: './modules/auth.module#AuthModule'
+  }
+  ];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    CommonModule,
+    HttpClientModule,
+    FormsModule,
+    AmexioWidgetModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes),
+    SharedlibModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
