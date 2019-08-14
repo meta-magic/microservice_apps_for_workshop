68026ba7d8195295b37e01d21a8cd5b1de1445a7
datsgawas <dattaram.gawas@metamagic.in>
2019-08-14 11:45:00 +0530

    order UI run standalone

--- order/order_ui/src/app/app.module.ts
+++ order/order_ui/src/app/app.module.ts
@@ -3,9 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { Routes, RouterModule } from '@angular/router';
+import {HttpClientModule} from "@angular/common/http";
+import {CookieService} from "ngx-cookie-service";
+import {AmexioWidgetModule} from "amexio-ng-extensions";
 
 
 const routes: Routes = [
+  {
+    path:'', redirectTo: 'order', pathMatch:'full'
+  },
   {
     path : 'order', loadChildren: './modules/orders.module#OrdersModule'
   }
@@ -16,10 +22,10 @@ const routes: Routes = [
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, HttpClientModule, AmexioWidgetModule,
     RouterModule.forRoot(routes, { useHash : true})
   ],
-  providers: [],
+  providers: [CookieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- order/order_ui/src/app/modules/orders.module.ts
+++ order/order_ui/src/app/modules/orders.module.ts
@@ -3,7 +3,7 @@
  */
 
 
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
 import {OrderslibModule} from "orderslib";
 
 @NgModule({
